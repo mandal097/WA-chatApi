bb7877a89e7f6b10d0566420d1c59df906c2622f
amarnath <amarnathmandal097@gmail.com>
2022-12-03 10:34:37 +0530

    creating search endpoint for searching for groups by groupName

--- src/routes/apis/Groups/_router.js
+++ src/routes/apis/Groups/_router.js
@@ -3,12 +3,14 @@ const auth = require('../../../middleware/auth');
 
 const adminRoutes = require('./Admin/_router');
 const getGroup = require('./getGroup');
+const searchedGroups = require('./searchGroup');
 const getAllGroups = require('./getAllGroups');
 
 
 
 router.use('/groups', auth, adminRoutes); //admin routes
 
+router.use('/groups', auth, searchedGroups)  //searching group by group name
 router.use('/groups', auth, getGroup)  //getting group by id
 router.use('/groups', auth, getAllGroups)  //getting group by id
 
---
+++ src/routes/apis/Groups/searchGroup.js
@@ -0,0 +1,25 @@
+const Group = require('../../../models/Group');
+
+const router = require('express').Router();
+
+router.get('/search', async (req, res) => {
+    const query = req.query.q;
+    const count = req.query.count;
+    try {
+        const groups = await Group.find({
+            groupName: { $regex: query, $options: '$i', }
+        }).limit(count)
+        res.json({
+            status: 'success',
+            data: groups
+        })
+    } catch (error) {
+        res.status(500).json({
+            status: 'err',
+            message: 'Something went wrong',
+            error
+        })
+    }
+})
+
+module.exports = router;
