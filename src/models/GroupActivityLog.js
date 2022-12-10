df741891ccd196d12b3fffe6217d4871dac337db
amarnath <amarnathmandal097@gmail.com>
2022-12-10 20:29:25 +0530

    model created for group activities logs

---
+++ src/models/GroupActivityLog.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+
+const groupActivityLogSchema = new mongoose.Schema({
+    groupId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Group",
+        required: true,
+    },
+    adminId: { //who takes actions in group 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    title: { // title of the acitvity log
+        type: String,
+        required: true,
+    },
+    note: {// about the acitivity
+        type: String,
+    },
+},
+    { timestamps: true }
+);
+
+const GroupActivityLog = new mongoose.model('GroupActivityLog', groupActivityLogSchema);
+
+module.exports = GroupActivityLog;
